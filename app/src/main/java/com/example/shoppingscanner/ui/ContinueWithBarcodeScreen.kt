453b74a400394430dd7b7af875cb9cdd2469844b
Neslişah Çelek <neslisah.celek@outlook.com>
2023-07-27 13:50:39 +0300

    Complete design

--- app/src/main/java/com/example/shoppingscanner/ui/ContinueWithBarcodeScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/ui/ContinueWithBarcodeScreen.kt
@@ -1,15 +1,76 @@
 package com.example.shoppingscanner.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.shoppingscanner.R
+import com.example.shoppingscanner.Screen
+import com.example.shoppingscanner.compose.ShopButtons
+import com.example.shoppingscanner.compose.ShopTexts
 
 @Composable
-fun ContinueWithBarcodeScreen() {
+fun ContinueWithBarcodeScreen(
+    navController: NavController
+) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+    ){
+        Card (
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .padding(10.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = CardDefaults.cardElevation(4.dp),
+            colors = CardDefaults.cardColors(Color.LightGray),
+            content = {
+                ShopTexts.BodyRegular(
+                    text = stringResource(R.string.continue_info),
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(60.dp))
+
+        ShopButtons.Primary(
+            text = stringResource(R.string.continue_with_barcode),
+            onClick = {
+                navController.navigate(Screen.BarcodeScannerScreen.route) },
+        )
+
+    }
 
 }
 
 @Composable
 @Preview(showBackground = true)
 fun ContinueWithBarcodeScreenPreview() {
-    ContinueWithBarcodeScreen()
+    ContinueWithBarcodeScreen(navController = rememberNavController())
 }
--- app/src/main/java/com/example/shoppingscanner/ui/DontWaitScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/ui/DontWaitScreen.kt
@@ -1,13 +1,29 @@
 package com.example.shoppingscanner.ui
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -17,30 +33,53 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.shoppingscanner.R
 import com.example.shoppingscanner.Screen
+import com.example.shoppingscanner.compose.ShopButtons
+import com.example.shoppingscanner.ui.theme.PurplePrimary
+
 
 @Composable
 fun DontWaitScreen(
     navController: NavController
 ) {
-    Column(Modifier.fillMaxSize()) {
-        Text(
-            text = stringResource(R.string.dont_wait),
-            style = MaterialTheme.typography.headlineMedium,
-            color = Color.Black
-        )
-        Button(
-            onClick = {Modifier.clickable { 
-                navController.navigate(Screen.ContinueWithBarcodeScreen.route)
-            } },
-            shape = RoundedCornerShape(20.dp)
-        )
-        {
-            Text(text = stringResource(R.string.dont_wait))
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
 
+        ShopButtons.Primary(
+            text = stringResource(R.string.dont_wait),
+            onClick = {
+                navController.navigate(Screen.ContinueWithBarcodeScreen.route) },
+        )
+
+        if (navController.currentBackStackEntry?.destination?.route == Screen.DontWaitScreen.route){
+            Image(
+                imageVector = Icons.Default.List,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(30.dp)
+                    .padding(start = 8.dp)
+            )
+        }
+
+
+        LazyColumn(){
+            items(10){
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(start = 8.dp),
+                    tint = PurplePrimary
+                )
+            }
         }
 
     }
-    // Spacer(modifier = Modifier.weight(1f))
 }
 
 @Composable
