b3d9b08d32653f1db4338a6e3482177dc8574bfd
Neslişah Çelek <neslisah.celek@outlook.com>
2023-07-31 15:33:45 +0300

    Update Constants.kt

--- app/src/main/java/com/example/shoppingscanner/util/Constants.kt
+++ app/src/main/java/com/example/shoppingscanner/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.shoppingscanner.util
 
 object Constants {
-    const val API_KEY = "4mytyij54nnc6d4regr8fi7quy2qf4"
+    const val API_KEY = "3nywf7rropykmux7dklbpewef4m9vk"
     const val BASE_URL = "https://api.barcodelookup.com/v3/"
 }
