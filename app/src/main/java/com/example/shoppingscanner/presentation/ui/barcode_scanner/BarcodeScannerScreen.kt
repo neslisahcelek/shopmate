e4d92c6a94d4f9a1f7e8eda3be8b7eddd691c7e1
Neslişah Çelek <neslisah.celek@outlook.com>
2023-07-31 15:32:50 +0300

    Change barcode scanner method

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerScreen.kt
@@ -1,15 +1,9 @@
 package com.example.shoppingscanner.presentation.ui.barcode_scanner
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.util.Size
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -26,11 +20,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
@@ -43,14 +34,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.shoppingscanner.R
 import com.example.shoppingscanner.presentation.ui.Screen
 import com.example.shoppingscanner.presentation.ui.theme.PurplePrimary
-import com.example.shoppingscanner.presentation.viewmodel.ProductViewModel
 import com.example.shoppingscanner.util.ShopButtons
 import com.example.shoppingscanner.util.ShopTexts
 
@@ -64,30 +51,10 @@ fun BarcodeScannerScreen(
     val state = viewModel.state.value
     val barcodeNumber = "3614272049529"
 
-    var code by remember {
-        mutableStateOf("")
-    }
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(context)
-    }
-    var hasCamPermission by remember {
-        mutableStateOf(
-            ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED
-        )
-    }
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission(),
-        onResult = { granted ->
-            hasCamPermission = granted
-        }
-    )
-    LaunchedEffect(key1 = true) {
-        launcher.launch(Manifest.permission.CAMERA)
+    val cameraController = remember {
+        LifecycleCameraController(context)
     }
 
     Column(
@@ -96,51 +63,23 @@ fun BarcodeScannerScreen(
     ) {
         Column(
             modifier = Modifier
-                .fillMaxHeight(0.5f)
+                .height(400.dp)
                 .fillMaxWidth(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (hasCamPermission) {
-                AndroidView(
-                    factory = { context ->
-                        val previewView = PreviewView(context)
-                        val preview = Preview.Builder().build()
-                        val selector = CameraSelector.Builder()
-                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                            .build()
-                        preview.setSurfaceProvider(previewView.surfaceProvider)
-                        val imageAnalysis = ImageAnalysis.Builder()
-                            .setTargetResolution(
-                                Size(
-                                    previewView.width,
-                                    previewView.height
-                                )
-                            )
-                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
-                            .build()
-                        imageAnalysis.setAnalyzer(
-                            ContextCompat.getMainExecutor(context),
-                            BarcodeAnalyzer { result ->
-                                code = result
-                                viewModel.getDataFromAPI(barcodeNumber)
-                            }
-                        )
-                        try {
-                            cameraProviderFuture.get().bindToLifecycle(
-                                lifecycleOwner,
-                                selector,
-                                preview,
-                                imageAnalysis
-                            )
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                        previewView
-                    },
-                    modifier = Modifier.height(200.dp)
-                )
-            }
+            AndroidView(
+                factory = { context ->
+                    PreviewView(context).apply {
+                        layoutParams = LinearLayout.LayoutParams(MATCH_PARENT,WRAP_CONTENT)
+                        scaleType = PreviewView.ScaleType.FILL_CENTER
+                    }.also {previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                },
+                modifier = Modifier.height(200.dp)
+            )
         }
             Column(
                 modifier = Modifier
@@ -194,16 +133,16 @@ fun BarcodeScannerScreen(
         Column (
             modifier = Modifier
                 .fillMaxHeight()
-                .padding(bottom=10.dp),
-            verticalArrangement = Arrangement.Bottom
+                .padding(bottom = 10.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.End
         ){
             ShopTexts.BodyBold(
-                text = stringResource(R.string.price_sum) + "${state.product?.price.toString()}",
+                text = stringResource(R.string.price_sum) + "${state.product?.price.toString()} ₺",
                 fontSize = 12.sp,
                 textAlign = TextAlign.End,
                 modifier = Modifier
-                    .padding(5.dp, end = 30.dp)
-                    .fillMaxWidth(0.9f)
+                    .padding(5.dp, end = 40.dp)
             )
             Row(
                 modifier = Modifier
@@ -215,14 +154,15 @@ fun BarcodeScannerScreen(
                 ShopButtons.Small(
                     text = stringResource(R.string.add_to_cart),
                     onClick = {
-                        viewModel.getDataFromAPI(barcodeNumber) //code
+                        viewModel.getBarcode()
+                        //viewModel.getDataFromAPI(barcodeNumber) //code
                     },
                 )
 
                 ShopButtons.Small(
                     text = stringResource(R.string.buy_now),
                     onClick = {
-                        navController.navigate(Screen.CartScreen.route + "/" + "$barcodeNumber")
+                        navController.navigate(Screen.CartScreen.route)
                     },
                 )
 
@@ -233,110 +173,7 @@ fun BarcodeScannerScreen(
 
     }
 
-fun addToCart() {
 
-}
 
-@Composable
-@androidx.compose.ui.tooling.preview.Preview(showBackground = true)
-fun barcodePreview(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier
-                .height(300.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                bitmap = ImageBitmap.imageResource(R.drawable.barcode),
-                contentDescription = "barcode",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth()
-            )
-        }
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth()
-                    .padding(start = 25.dp, end = 25.dp, top = 15.dp, bottom = 15.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.CenterHorizontally,
-
-                ) {
-                Column(
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                ) {
-                    Row {
-                        ShopTexts.BodyBold(
-                            text = "Ürün: ",
-                            modifier = Modifier.padding(end = 5.dp),
-                            textAlign = TextAlign.Start
-                        )
-                        ShopTexts.BodyRegular(
-                            text = "dhtdjjt",
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Start
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Row {
-                        ShopTexts.BodyBold(
-                            text = "Fiyat: ",
-                            modifier = Modifier.padding(end = 5.dp),
-                            textAlign = TextAlign.Start
-                        )
-                        ShopTexts.BodyRegular(
-                            text = "djjddj}" + " ₺",
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Start
-                        )
-                    }
-                }
-
-                Column {
-                    ShopTexts.BodyBold(
-                        text = stringResource(R.string.price_sum) + "kgkds",
-                        fontSize = 12.sp,
-                        textAlign = TextAlign.End,
-                        modifier = Modifier
-                            .padding(5.dp, end = 30.dp)
-                            .fillMaxWidth(0.9f)
-                    )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp, bottom = 15.dp, top = 5.dp),
-                        horizontalArrangement = Arrangement.SpaceAround,
-                    ) {
-
-                        ShopButtons.Small(
-                            text = stringResource(R.string.add_to_cart),
-                            onClick = {
-
-                            },
-                        )
-
-                        ShopButtons.Small(
-                            text = stringResource(R.string.buy_now),
-                            onClick = {
-
-                            },
-                        )
-
-                    }
-
-                }
-
-            }
-        }
-
-    }
 
 
--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/ProductViewModel.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/ProductViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.shoppingscanner.presentation.viewmodel
+package com.example.shoppingscanner.presentation.ui.barcode_scanner
 
 import android.util.Log
 import androidx.compose.runtime.State
@@ -8,12 +8,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.shoppingscanner.data.remote.dto.Product
 import com.example.shoppingscanner.domain.model.CartProduct
+import com.example.shoppingscanner.domain.repository.BarcodeRepository
 import com.example.shoppingscanner.domain.usecase.GetProduct
 import com.example.shoppingscanner.presentation.ui.barcode_scanner.BarcodeScannerEvent
 import com.example.shoppingscanner.presentation.ui.barcode_scanner.BarcodeScannerState
 import com.example.shoppingscanner.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -22,16 +24,25 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(
-   private val getProductUseCase:GetProduct
+   private val getProductUseCase:GetProduct,
+    private val barcodeRepo:BarcodeRepository
 ) : ViewModel() {
 
     private val _state = mutableStateOf<BarcodeScannerState>(BarcodeScannerState())
     val state : State<BarcodeScannerState> = _state
 
-
-
-
-
+    fun getBarcode(){
+        println("getbarcode")
+        viewModelScope.launch {
+            barcodeRepo.scan().collect(){
+                println("getbarcode scan")
+                if(!it.isNullOrBlank()){
+                    println("barcode not null")
+                    getDataFromAPI(it)
+                }
+            }
+        }
+    }
     fun getDataFromAPI(barcode:String) {
         println("get data from api")
         getProductUseCase.executeGetProduct(barcode = barcode).onEach {
