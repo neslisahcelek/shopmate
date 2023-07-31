af1a7e87e67f96e463660504ccabbc3e020c01da
Neslişah Çelek <neslisah.celek@outlook.com>
2023-07-31 12:29:07 +0300

    Change design

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerScreen.kt
@@ -1,5 +1,16 @@
 package com.example.shoppingscanner.presentation.ui.barcode_scanner
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Size
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,20 +21,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.shoppingscanner.R
 import com.example.shoppingscanner.presentation.ui.Screen
+import com.example.shoppingscanner.presentation.ui.theme.PurplePrimary
 import com.example.shoppingscanner.presentation.viewmodel.ProductViewModel
 import com.example.shoppingscanner.util.ShopButtons
 import com.example.shoppingscanner.util.ShopTexts
@@ -33,112 +59,284 @@ import com.example.shoppingscanner.util.ShopTexts
 fun BarcodeScannerScreen(
     navController: NavController,
     viewModel : ProductViewModel
-    ){
+    ) {
 
     val state = viewModel.state.value
-
     val barcodeNumber = "3614272049529"
 
+    var code by remember {
+        mutableStateOf("")
+    }
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(context)
+    }
+    var hasCamPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted ->
+            hasCamPermission = granted
+        }
+    )
+    LaunchedEffect(key1 = true) {
+        launcher.launch(Manifest.permission.CAMERA)
+    }
 
     Column(
-        modifier= Modifier
+        modifier = Modifier
             .fillMaxSize()
     ) {
         Column(
-            modifier= Modifier
-                .fillMaxHeight(0.4f)
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
                 .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Image(
-                bitmap = ImageBitmap.imageResource(id = R.drawable.barcode),
-                contentDescription = "barcode",
-                contentScale = ContentScale.Fit,
-
-
-            )
-        }
-        Column(
-            modifier= Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
-                .padding(start = 25.dp, end = 25.dp, top = 15.dp, bottom = 15.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally,
-
         ) {
+            if (hasCamPermission) {
+                AndroidView(
+                    factory = { context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(
+                                Size(
+                                    previewView.width,
+                                    previewView.height
+                                )
+                            )
+                            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+                        imageAnalysis.setAnalyzer(
+                            ContextCompat.getMainExecutor(context),
+                            BarcodeAnalyzer { result ->
+                                code = result
+                                viewModel.getDataFromAPI(barcodeNumber)
+                            }
+                        )
+                        try {
+                            cameraProviderFuture.get().bindToLifecycle(
+                                lifecycleOwner,
+                                selector,
+                                preview,
+                                imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        previewView
+                    },
+                    modifier = Modifier.height(200.dp)
+                )
+            }
+        }
             Column(
-                verticalArrangement = Arrangement.SpaceEvenly,
-            ) {
-                Row {
-                    ShopTexts.BodyBold(
-                        text = "Ürün: ",
-                        modifier = Modifier.padding(end = 5.dp),
-                        textAlign = TextAlign.Start
-                    )
-                    ShopTexts.BodyRegular(
-                        text = "${state.product!!.title}",
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Start
-                    )
-                }
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 5.dp, bottom = 15.dp),
 
-                Spacer(modifier = Modifier.height(10.dp))
-                Row {
-                    ShopTexts.BodyBold(
-                        text = "Fiyat: ",
-                        modifier = Modifier.padding(end = 5.dp),
-                        textAlign = TextAlign.Start
-                    )
-                    ShopTexts.BodyRegular(
-                        text = "${state.product!!.price}" +" ₺",
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Start
-                    )
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+                ) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .size(40.dp)
+                        .padding(start = 8.dp),
+                    tint = PurplePrimary
+                )
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    Row {
+                        ShopTexts.BodyBold(
+                            text = "Ürün: ",
+                            modifier = Modifier.padding(end = 5.dp),
+                            textAlign = TextAlign.Start
+                        )
+                        ShopTexts.BodyRegular(
+                            text = "${state.product!!.title}",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Row {
+                        ShopTexts.BodyBold(
+                            text = "Fiyat: ",
+                            modifier = Modifier.padding(end = 5.dp),
+                            textAlign = TextAlign.Start
+                        )
+                        ShopTexts.BodyRegular(
+                            text = "${state.product!!.price}" + " ₺",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
+                    }
                 }
             }
+        Column (
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(bottom=10.dp),
+            verticalArrangement = Arrangement.Bottom
+        ){
+            ShopTexts.BodyBold(
+                text = stringResource(R.string.price_sum) + "${state.product?.price.toString()}",
+                fontSize = 12.sp,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .padding(5.dp, end = 30.dp)
+                    .fillMaxWidth(0.9f)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp, bottom = 15.dp, top = 5.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+            ) {
 
-            Column {
-                ShopTexts.BodyBold(
-                    text = stringResource(R.string.price_sum) + "${state.product?.price.toString()}",
-                    fontSize = 12.sp,
-                    textAlign = TextAlign.End,
-                    modifier = Modifier
-                        .padding(5.dp, end = 30.dp)
-                        .fillMaxWidth(0.9f)
+                ShopButtons.Small(
+                    text = stringResource(R.string.add_to_cart),
+                    onClick = {
+                        viewModel.getDataFromAPI(barcodeNumber) //code
+                    },
                 )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp, bottom = 15.dp, top = 5.dp),
-                    horizontalArrangement = Arrangement.SpaceAround,
-                ) {
 
-                    ShopButtons.Small(
-                        text = stringResource(R.string.add_to_cart),
-                        onClick = {
-                            viewModel.onEvent(BarcodeScannerEvent.GetData(barcodeNumber))
-                        },
-                    )
-
-                    ShopButtons.Small(
-                        text = stringResource(R.string.buy_now),
-                        onClick = {
-                            navController.navigate(Screen.CartScreen.route + "/12344")
-                        },
-                    )
-
-                }
+                ShopButtons.Small(
+                    text = stringResource(R.string.buy_now),
+                    onClick = {
+                        navController.navigate(Screen.CartScreen.route + "/" + "$barcodeNumber")
+                    },
+                )
 
             }
 
         }
-    }
+        }
 
-}
+    }
 
 fun addToCart() {
 
 }
+
+@Composable
+@androidx.compose.ui.tooling.preview.Preview(showBackground = true)
+fun barcodePreview(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.barcode),
+                contentDescription = "barcode",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            )
+        }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp, top = 15.dp, bottom = 15.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+                ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    Row {
+                        ShopTexts.BodyBold(
+                            text = "Ürün: ",
+                            modifier = Modifier.padding(end = 5.dp),
+                            textAlign = TextAlign.Start
+                        )
+                        ShopTexts.BodyRegular(
+                            text = "dhtdjjt",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Row {
+                        ShopTexts.BodyBold(
+                            text = "Fiyat: ",
+                            modifier = Modifier.padding(end = 5.dp),
+                            textAlign = TextAlign.Start
+                        )
+                        ShopTexts.BodyRegular(
+                            text = "djjddj}" + " ₺",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
+                    }
+                }
+
+                Column {
+                    ShopTexts.BodyBold(
+                        text = stringResource(R.string.price_sum) + "kgkds",
+                        fontSize = 12.sp,
+                        textAlign = TextAlign.End,
+                        modifier = Modifier
+                            .padding(5.dp, end = 30.dp)
+                            .fillMaxWidth(0.9f)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp, bottom = 15.dp, top = 5.dp),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                    ) {
+
+                        ShopButtons.Small(
+                            text = stringResource(R.string.add_to_cart),
+                            onClick = {
+
+                            },
+                        )
+
+                        ShopButtons.Small(
+                            text = stringResource(R.string.buy_now),
+                            onClick = {
+
+                            },
+                        )
+
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
 
