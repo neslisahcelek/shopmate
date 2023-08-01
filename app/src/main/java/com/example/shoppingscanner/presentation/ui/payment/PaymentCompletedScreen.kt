2d08c3510db6cdb378b07a4ef8025c2533516082
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-01 16:13:28 +0300

    Add box for images

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/payment/PaymentCompletedScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/payment/PaymentCompletedScreen.kt
@@ -10,15 +10,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -52,14 +59,32 @@ fun PaymentCompletedScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceAround,
 ) {
-        Image(
-            bitmap = ImageBitmap.imageResource(R.drawable.tick),
-            contentDescription = "tick",
-            contentScale = ContentScale.Fit,
+        Box(
             modifier = Modifier
                 .fillMaxHeight(0.4f)
                 .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+
+
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.purpledot),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
             )
+            Image(
+                imageVector = Icons.Default.CheckCircle,
+                contentDescription = "tick",
+                modifier = Modifier
+                    .size(140.dp)
+                    .align(Alignment.Center)
+                    ,
+                colorFilter = ColorFilter.tint(Green)
+
+            )
+        }
+
         ShopTexts.Title1Bold(
             text = stringResource(R.string.payment_successfull),
             textAlign = TextAlign.Center,
