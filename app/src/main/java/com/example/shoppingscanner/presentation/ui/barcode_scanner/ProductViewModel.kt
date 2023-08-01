b4fd8aab9279091a5261b33784f58648e521bf17
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-01 16:14:12 +0300

    Optimize addtocart function

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/ProductViewModel.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/ProductViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.shoppingscanner.presentation.ui.barcode_scanner
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,7 +17,9 @@ import com.example.shoppingscanner.domain.usecase.GetProduct
 import com.example.shoppingscanner.presentation.ui.barcode_scanner.BarcodeScannerEvent
 import com.example.shoppingscanner.presentation.ui.barcode_scanner.BarcodeScannerState
 import com.example.shoppingscanner.util.Resource
+import com.example.shoppingscanner.util.showToast
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
@@ -24,14 +30,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(
-   private val getProductUseCase:GetProduct,
+    @ApplicationContext private val context: Context,
+    private val getProductUseCase:GetProduct,
     private val barcodeRepo:BarcodeRepository
 ) : ViewModel() {
 
     private val _state = mutableStateOf<BarcodeScannerState>(BarcodeScannerState())
     val state : State<BarcodeScannerState> = _state
 
-    private val product = _state.value.product
     fun getBarcode(){
         println("getbarcode")
         viewModelScope.launch {
@@ -40,6 +46,8 @@ class ProductViewModel @Inject constructor(
                 if(!it.isNullOrBlank()){
                     println("barcode not null")
                     getDataFromAPI(it)
+                }else{
+                    showToast(context,"Barkod bulunamadı.")
                 }
             }
         }
@@ -49,12 +57,13 @@ class ProductViewModel @Inject constructor(
         getProductUseCase.executeGetProduct(barcode = barcode).onEach {
             when(it){
                 is Resource.Success -> {
-                    println("get data from api success")
-                    _state.value = BarcodeScannerState(product = it.data?.get(0))
+                    _state.value = _state.value.copy(product = it.data?.get(0))
+                    println("get data from api success new product: " + state.value.product!!.title)
                 }
 
                 is Resource.Error -> {
                     _state.value = state.value.copy(error = it.message)
+                    showToast(context,"Lütfen tekrar deneyin.")
                     println("get data from api error" + it.message)
                 }
                 is Resource.Loading -> {
@@ -73,14 +82,18 @@ class ProductViewModel @Inject constructor(
     }
 
     fun addToCart() {
+        val product = state.value.product
         val updatedProduct = product?.copy(quantity = (product.quantity + 1))
+        println("updated product: " + updatedProduct!!.title)
 
         _state.value = state.value.copy(product = updatedProduct)
 
         updatedProduct?.let {
             _state.value.totalPrice = it.price?.toDouble()?.times(it.quantity)!!
+            println("updated total: " + _state.value.totalPrice)
         }
 
     }
+
 }
 
