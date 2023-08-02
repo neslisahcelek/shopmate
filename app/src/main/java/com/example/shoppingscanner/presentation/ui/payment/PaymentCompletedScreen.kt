f1fd54cd7fe83a27976a55f8a4373fc08b357fd7
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-02 13:32:48 +0300

    Change icon

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/payment/PaymentCompletedScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/payment/PaymentCompletedScreen.kt
@@ -1,6 +1,8 @@
 package com.example.shoppingscanner.presentation.ui.payment
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,9 +13,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.twotone.CheckCircle
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -22,12 +27,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.shoppingscanner.R
@@ -38,6 +45,7 @@ import com.example.shoppingscanner.domain.model.CartProduct
 import com.example.shoppingscanner.presentation.ui.Screen
 import com.example.shoppingscanner.presentation.ui.barcode_scanner.ProductViewModel
 import com.example.shoppingscanner.presentation.ui.theme.Green
+import com.example.shoppingscanner.presentation.ui.theme.PurpleGrey40
 
 
 @Composable
@@ -71,15 +79,23 @@ fun PaymentCompletedScreen(
                 painter = painterResource(id = R.drawable.purpledot),
                 contentDescription = "",
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.tint(PurpleGrey40)
+
+            )
+            Box(
+                modifier = Modifier.size(120.dp)
+                    .align(Alignment.Center)
+                    .background(Color.White, shape = CircleShape)
             )
             Image(
                 imageVector = Icons.Default.CheckCircle,
                 contentDescription = "tick",
                 modifier = Modifier
-                    .size(140.dp)
+                    .size(180.dp)
                     .align(Alignment.Center)
-                    ,
+                ,
                 colorFilter = ColorFilter.tint(Green)
 
             )
@@ -106,5 +122,43 @@ fun PaymentCompletedScreen(
         )
 
         Spacer(modifier = Modifier.padding(bottom=8.dp))
+    }
+}
+
+@Preview
+@Composable
+fun imagePreview(){
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+
+
+
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.purpledot),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.tint(PurpleGrey40)
+
+        )
+        Box(
+            modifier = Modifier.size(120.dp)
+                .align(Alignment.Center)
+                .background(Color.White, shape = CircleShape)
+        )
+        Image(
+            imageVector = Icons.Default.CheckCircle,
+            contentDescription = "tick",
+            modifier = Modifier
+                .size(180.dp)
+                .align(Alignment.Center)
+            ,
+            colorFilter = ColorFilter.tint(Green)
+
+        )
     }
 }
--- app/src/main/java/com/example/shoppingscanner/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/theme/Color.kt
@@ -7,7 +7,7 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val PurplePrimary = Color(0xFF7F00FF)
-val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey40 = Color(0xFF7355A2)
 val Pink40 = Color(0xFF7D5260)
 
 val Green = Color(0xFF00B100)
