c3f35a57d818503c0947803b1d5dbdf8553c5024
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-02 11:08:55 +0300

    Add cartproducts list

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
@@ -4,7 +4,8 @@ import com.example.shoppingscanner.domain.model.CartProduct
 
 data class BarcodeScannerState(
     val isLoading: Boolean = false,
-    val error : String? = "",
-    val product : CartProduct? = CartProduct("1234","Nestle Çilekli Çikolata","15.99","",0),
+    val error: String? = "",
+    val cartProducts: List<CartProduct> = mutableListOf(),
+    val product: CartProduct? = CartProduct("1234","Nestle Çilekli Çikolata","15.0","",0),
     var totalPrice:Double = 0.0
 )
