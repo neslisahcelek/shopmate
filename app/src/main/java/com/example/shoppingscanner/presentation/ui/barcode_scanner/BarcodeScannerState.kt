e2e1b29a836fafc4d67abc0156ae58e59139ee35
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-09 17:06:17 +0300

    Update BarcodeScannerState.kt

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
@@ -1,12 +1,14 @@
 package com.example.shoppingscanner.presentation.ui.barcode_scanner
 
 import com.example.shoppingscanner.domain.dto.CartProduct
+import com.example.shoppingscanner.domain.dto.ListProduct
 
 data class BarcodeScannerState(
     val isLoading: Boolean = false,
     val error: String? = "",
     val cartProducts: List<CartProduct> = mutableListOf(),
-    val product: CartProduct? = CartProduct("1234","Nestle Çilekli Çikolata","15.0","",0),
+    val product: CartProduct? = CartProduct("1","Nestle Çilekli Çikolata","15.0","https://images.migrosone.com/sanalmarket/product/7018051/7018051-b87c99-1650x1650.jpg",0),
     var totalPrice:Double = 0.0,
-    var messageId : Int? = null
+    var messageId : Int? = null,
+    var missingProducts: List<ListProduct> = mutableListOf()
 )
