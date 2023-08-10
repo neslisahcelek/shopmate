73bda977f6ee1630dc19015e5358d1d3900d78ee
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-10 19:14:57 +0300

    Add shopping list button

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/productlist/ProductListScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/productlist/ProductListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.shoppingscanner.presentation.ui.productlist
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,11 +10,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,6 +32,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import com.example.shoppingscanner.R
 import com.example.shoppingscanner.component.ShopButtons
@@ -28,7 +40,9 @@ import com.example.shoppingscanner.component.ShopList
 import com.example.shoppingscanner.component.ShopTexts
 import com.example.shoppingscanner.domain.dto.ListProduct
 import com.example.shoppingscanner.presentation.ui.Screen
+import com.example.shoppingscanner.presentation.ui.barcode_scanner.ShoppingList
 import com.example.shoppingscanner.presentation.ui.base.BaseEvent
+import com.example.shoppingscanner.presentation.ui.theme.PurpleGrey80
 import com.example.shoppingscanner.presentation.ui.theme.PurplePrimary
 import com.example.shoppingscanner.util.showToast
 
@@ -41,6 +55,11 @@ fun ProductListScreen(
 
     val state by rememberUpdatedState(newValue = viewModel.state.value)
     val context = LocalContext.current
+
+    val shoppingList = viewModel.shoppingListState.value.shoppingList
+    var isShoppingListVisible by remember {
+        mutableStateOf(false)
+    }
 
     state.messageId?.let {
         showToast(
@@ -70,8 +89,9 @@ fun ProductListScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(
-                        top=10.dp,
-                        start = 10.dp),
+                        top = 10.dp,
+                        start = 10.dp
+                    ),
                 viewModel = viewModel
             )
 
@@ -94,15 +114,46 @@ fun ProductListScreen(
                     )
                 }
 
-                ShopButtons.Small(
-                    text = stringResource(R.string.buy_now),
-                    onClick = {
-                        navController.navigate(Screen.ContinueWithBarcodeScreen.route)
-                    },
-                    modifier = Modifier
+                ConstraintLayout (
+                    modifier = Modifier.fillMaxWidth()
                         .align(Alignment.BottomCenter)
-                        .padding(bottom = 15.dp)
-                )
+                ){
+                    val (icon, button) = createRefs()
+
+                    ShopButtons.Primary(
+                        text = stringResource(R.string.continue_with_barcode),
+                        onClick = {
+                            navController.navigate(Screen.BarcodeScannerScreen.route)
+                        },
+                        modifier = Modifier
+                            .constrainAs(button){
+                                bottom.linkTo(parent.bottom)
+                                centerHorizontallyTo(parent)
+                            }
+                            .width(250.dp)
+                            .padding(bottom = 15.dp)
+                    )
+                    IconButton(
+                        onClick = { isShoppingListVisible = !isShoppingListVisible },
+                        modifier = Modifier
+                            .constrainAs(icon){
+                                end.linkTo(parent.end)
+                                start.linkTo(button.end)
+                            }
+                            .background(PurpleGrey80, CircleShape)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.List,
+                            tint = PurplePrimary,
+                            contentDescription = null
+                        )
+                    }
+                }
+
+                if (isShoppingListVisible) {
+                    shoppingList?.let { ShoppingList(list = it) }
+                }
+
             }
 
         }
@@ -110,4 +161,19 @@ fun ProductListScreen(
 
     }
 }
+@Composable
+fun ShoppingList(
+    list : List<ListProduct>
+) {
+    list.let {
+        ShopList.ShoppingProductList(
+            productList = it,
+            modifier = Modifier
+                .border(1.dp, PurplePrimary, RoundedCornerShape(10.dp))
+                .background(PurpleGrey80)
+                .fillMaxWidth()
+        )
+    }
+}
+
 
