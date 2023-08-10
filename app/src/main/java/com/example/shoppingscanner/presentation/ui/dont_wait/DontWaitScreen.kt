2c0e36ce3103008f6c9ff5e4808c07b9c7ae9360
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-10 19:14:32 +0300

    Change info text

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/dont_wait/DontWaitScreen.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/dont_wait/DontWaitScreen.kt
@@ -2,19 +2,29 @@ package com.example.shoppingscanner.presentation.ui.dont_wait
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.shoppingscanner.R
 import com.example.shoppingscanner.presentation.ui.Screen
 import com.example.shoppingscanner.component.ShopButtons
+import com.example.shoppingscanner.component.ShopTexts
 
 
 @Composable
@@ -24,13 +34,31 @@ fun DontWaitScreen(
     Column(
         Modifier
             .fillMaxSize()
-            .padding(top = 200.dp),
+            .padding(top = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
-    ) {
+
+        ){
+        Card (
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(10.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = CardDefaults.cardElevation(4.dp),
+            colors = CardDefaults.cardColors(Color.LightGray),
+            content = {
+                ShopTexts.BodyRegular(
+                    text = stringResource(R.string.continue_info),
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(20.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(60.dp))
 
         ShopButtons.Primary(
-            text = stringResource(R.string.dont_wait),
+            text = stringResource(R.string.show_products),
             onClick = {
                 navController.navigate(Screen.ProductListScreen.route) },
         )
