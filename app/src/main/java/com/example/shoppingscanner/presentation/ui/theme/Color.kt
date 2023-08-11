99c16178c1831fa87dcd0844786b50dade1e9b8b
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-11 17:49:39 +0300

    Update Color.kt

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.shoppingscanner.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Purple80 = Color(0xFAEADEFA)
+val PurpleGrey80 = Color(0xFFE5D9FA)
 val Pink80 = Color(0xFFEFB8C8)
 
 val PurplePrimary = Color(0xFF7F00FF)
