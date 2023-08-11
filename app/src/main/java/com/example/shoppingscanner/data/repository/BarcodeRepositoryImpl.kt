e618c03f7d745d641dea47ca30d9482230aeffbb
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-11 17:49:33 +0300

    Update BarcodeRepositoryImpl.kt

--- app/src/main/java/com/example/shoppingscanner/data/repository/BarcodeRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppingscanner/data/repository/BarcodeRepositoryImpl.kt
@@ -12,31 +12,22 @@ class BarcodeRepositoryImpl @Inject constructor(
     private val scanner : GmsBarcodeScanner
 ): BarcodeRepository{
 
-    var isPauseScan = false
     override fun scan(): Flow<String?> {
         return callbackFlow {
             scanner.startScan()
                 .addOnSuccessListener { barcode ->
-
-                        println("scan success")
-
                         launch {
                             send(getBarcodeValue(barcode))
                         }
-
-
             }
                 .addOnFailureListener {
-                    println("scan fail" + it)
                     it.printStackTrace()
                 }
-
             awaitClose()
         }
     }
 
     private fun getBarcodeValue(barcode: com.google.mlkit.vision.barcode.common.Barcode):String? {
-        println("barcode: " +barcode)
         return barcode.rawValue
     }
 
