b7ce0f6eefd0a7a1ccfe2fbaffed916adb1a85f2
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-14 19:37:18 +0300

    Add short and long toast functions

--- app/src/main/java/com/example/shoppingscanner/util/Toast.kt
+++ app/src/main/java/com/example/shoppingscanner/util/Toast.kt
@@ -3,6 +3,9 @@ package com.example.shoppingscanner.util
 import android.content.Context
 import android.widget.Toast
 
-fun showToast(context: Context, message:String){
-    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+fun showShortToast(context: Context, message:String){
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+fun showLongToast(context: Context, message:String){
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
