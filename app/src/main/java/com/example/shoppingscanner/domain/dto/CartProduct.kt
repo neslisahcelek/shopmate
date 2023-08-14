4313d0a4ad88a28bb69215f29e3df8e9a69537d6
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-14 19:30:35 +0300

    Make barcode number nullable

--- app/src/main/java/com/example/shoppingscanner/domain/dto/CartProduct.kt
+++ app/src/main/java/com/example/shoppingscanner/domain/dto/CartProduct.kt
@@ -1,9 +1,9 @@
 package com.example.shoppingscanner.domain.dto
 
 data class CartProduct(
-    val barcode_number: String,
+    val barcode_number: String?,
     val title: String?,
-    val price: String?,
+    val price: String? = "10.0",
     val image:String?,
     var quantity: Int
 )
