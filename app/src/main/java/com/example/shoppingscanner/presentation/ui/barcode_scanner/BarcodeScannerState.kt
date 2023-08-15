6704639553268e96fa5606881773fecc3deccf77
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-15 13:02:52 +0300

    Make nullable

--- app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
+++ app/src/main/java/com/example/shoppingscanner/presentation/ui/barcode_scanner/BarcodeScannerState.kt
@@ -6,9 +6,9 @@ import com.example.shoppingscanner.domain.dto.ListProduct
 data class BarcodeScannerState(
     val isLoading: Boolean = false,
     val error: String? = "",
-    val cartProducts: List<CartProduct> = mutableListOf(),
+    val cartProducts: List<CartProduct?> = mutableListOf(),
     val product: CartProduct? = CartProduct("1","Nestle Çilekli Çikolata","15.0","https://images.migrosone.com/sanalmarket/product/7018051/7018051-b87c99-1650x1650.jpg",0),
     var totalPrice:Double = 0.0,
-    var messageId : Int? = null,
+    var messageId: Int? = null,
     var missingProducts: List<ListProduct> = mutableListOf()
 )
