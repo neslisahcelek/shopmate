36867dd47189289fa40a1e406179f193fa42b107
Neslişah Çelek <neslisah.celek@outlook.com>
2023-08-16 09:46:21 +0300

    Update api key

--- app/src/main/java/com/example/shoppingscanner/util/Constants.kt
+++ app/src/main/java/com/example/shoppingscanner/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.shoppingscanner.util
 
 object Constants {
-    const val API_KEY = "i7cqzgxfqym3uzvnny8otzpoanfdcm"
+    const val API_KEY = "tfnx36nqe3y3da1df3r9nym9gpgbhe"
     const val BASE_URL = "https://api.barcodelookup.com/v3/"
     const val barcodeNumber = "3614272049529"
 }
